786d031233fe8fd4ed8e7cf93a5b4a952927e418
yoshoku <yoshoku@outlook.com>
2019-08-12 16:59:52 +0900

    :articulated_lorry: Change IO module configuration

---
+++ ext/magro/io.h
@@ -0,0 +1,18 @@
+#ifndef MAGRO_IO_H
+#define MAGRO_IO_H 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <setjmp.h>
+#include <png.h>
+#include <jpeglib.h>
+
+#include <ruby.h>
+#include <numo/narray.h>
+#include <numo/template.h>
+
+void init_io_module();
+
+#endif /* MAGRO_IO_H */
--- ext/magro/magro.h
+++ ext/magro/magro.h
@@ -1,15 +1,8 @@
 #ifndef MAGRO_H
 #define MAGRO_H 1
 
-#include <math.h>
-#include <string.h>
-
-#include <setjmp.h>
-#include <png.h>
-#include <jpeglib.h>
-
 #include <ruby.h>
-#include <numo/narray.h>
-#include <numo/template.h>
+
+#include "io.h"
 
 #endif /* MAGRO_H */
