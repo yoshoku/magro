c9e55d83404f70d27ff2717fba2d015b7df55fe5
yoshoku <yoshoku@outlook.com>
2019-08-15 23:04:14 +0900

    :books: Add descriptions about raised errors

--- lib/magro/io.rb
+++ lib/magro/io.rb
@@ -9,6 +9,9 @@ module Magro
     # @param filename [String] Path to image file to be loaded.
     #   Currently, the following file formats are support:
     #   Portbale Network Graphics (*.png) and JPEG files (*.jpeg, *.jpg, *jpe).
+    # @raise [ArgumentError] This error is raised when filename is not String.
+    # @raise [IOError] This error is raised when failed to read image file.
+    # @raise [NoMemoryError] If memory allocation of image data fails, this error is raised.
     # @return [Numo::UInt8] (shape: [height, width, n_channels]) Loaded image.
     def imread(filename)
       raise ArgumentError, 'Expect class of filename to be String.' unless filename.is_a?(String)
@@ -22,6 +25,9 @@ module Magro
     #   Portbale Network Graphics (*.png) and JPEG files (*.jpeg, *.jpg, *jpe).
     # @param image [Numo::UInt8] (shape: [height, width, n_channels]) Image data to be saved.
     # @param quality [Integer] Quality parameter of jpeg image that takes a value between 0 to 100.
+    # @raise [ArgumentError] If filename is not String or image is not Numo::NArray, this error is raised.
+    # @raise [IOError] This error is raised when failed to read image file.
+    # @raise [NoMemoryError] If memory allocation of image data fails, this error is raised.
     # @return [Boolean] true if file save is successful.
     def imsave(filename, image, quality: nil)
       raise ArgumentError, 'Expect class of filename to be String.' unless filename.is_a?(String)
