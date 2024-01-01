754aeec8675e988ab4d655a4056828368d795d42
yoshoku <yoshoku@outlook.com>
2024-01-01 18:52:30 +0900

    docs: update copyright year

--- ext/magro/magro.c
+++ ext/magro/magro.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019-2022 Atsushi Tatsuma
+ * Copyright (c) 2019-2024 Atsushi Tatsuma
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
